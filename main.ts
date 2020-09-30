f0ffdef05725c06432edb78d1eb17242fd206e1c
Vaughn-Hatherall <71399950+Vaughn-Hatherall@users.noreply.github.com>
2020-09-30 12:57:21 -0400

    Final commit

--- main.ts
+++ main.ts
@@ -1,7 +1,24 @@
+let strip: neopixel.Strip = null
 // Created By: Vaughn Hatherall
 // Created on: September 30
 // 
 // This program Acts like a traffic light
 basic.forever(function () {
-	
+    strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+    strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+    strip.show()
+    basic.pause(2000)
+    basic.pause(1000)
+    strip.clear()
+    strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+    strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
+    strip.show()
+    basic.pause(1000)
+    strip.clear()
+    strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+    strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+    strip.show()
+    basic.pause(2000)
+    basic.pause(1000)
+    strip.clear()
 })
